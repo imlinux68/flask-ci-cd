acdd282b6c5ca3a67b91882fff5004a1dd58c241
im linux <imlinux68@gmail.com>
2023-05-23 12:21:22 +0300

    changed to 5000

--- CI_CD_Project/app/run.py
+++ CI_CD_Project/app/run.py
@@ -1,6 +1,8 @@
-from flask import Flask, render_template, session, request, redirect, jsonify 
+from flask import Flask, render_template, session, request, redirect, jsonify, Response
+from crypt import methods
 import random 
 import math
+from flask_oidc import OpenIDConnect
 
 app = Flask(__name__)
 app.secret_key = 'thisIsSecret'
@@ -23,67 +25,67 @@ oidc = OpenIDConnect(app)
 @app.route('/')
 @oidc.require_login
 def index():
- return redirect('/home')
+	return redirect('/home')
 
 @app.route('/home')
 def home():
- session["victory"] = 0
- return render_template('guess.html')
+	session["victory"] = 0
+	return render_template('guess.html')
 
 @app.route('/process', methods=['POST'])
 def process():
- if request.form.get('low'):
-  low = int(request.form['low'])
-  high = int(request.form['high'])
-  randomValue = int(random.randint(low, high))
-  Guesses = round(math.log(randomValue + 1, 2))
+	if request.form.get('low'):
+		low = int(request.form['low'])
+		high = int(request.form['high'])
+		randomValue = int(random.randint(low, high))
+		Guesses = round(math.log(randomValue + 1, 2))
 
-  if high - low < 9:
-   Message = ("Thats to easy! The offset between the numbers needs to atleast be 10")
-   return jsonify({'inputError' : Message})
-  else:
-   session['random_value'] = randomValue
-   session['guess_value'] = Guesses
-   guessLeft = str(session['guess_value'])
-   print(session)
+		if high - low < 9:
+			Message = ("Thats to easy! The offset between the numbers needs to atleast be 10")
+			return jsonify({'inputError' : Message})
+		else:
+			session['random_value'] = randomValue
+			session['guess_value'] = Guesses
+			guessLeft = str(session['guess_value'])
+			print(session)
 
-   Message = ("You've only " + str(Guesses)  + " chances to guess the integer!")
-   return jsonify({'set' : Message, 'lowest' : low, 'highest' : high, 'guesses' : guessLeft})
+			Message = ("You've only " + str(Guesses)  + " chances to guess the integer!")
+			return jsonify({'set' : Message, 'lowest' : low, 'highest' : high, 'guesses' : guessLeft})
 
- if request.form.get('guess'):
-  guess = int(request.form['guess'])
+	if request.form.get('guess'):
+		guess = int(request.form['guess'])
 
-  if session['random_value'] == guess:
-   session['victory'] += 1
-   score = session['victory']
-   Message = ("Correct! The number was: " + str(guess))
-   return jsonify({'won' : Message, 'score': score})
+		if session['random_value'] == guess:
+			session['victory'] += 1
+			score = session['victory']
+			Message = ("Correct! The number was: " + str(guess))
+			return jsonify({'won' : Message, 'score': score})
 
-  elif session['random_value'] > guess:
-   session['guess_value'] -= 1
-   if session['guess_value'] == 0:
-    session['victory'] -= 1
-    score = session['victory']
-    Message = ("Sadly you ran out of guesses, the number was " + str(session['random_value']) + ". please try again ")
-    return jsonify({'error' : Message, 'guesses' : '0', 'score': score})
-   else:
-    Message = (str(guess) + " is to low! You have " + str(session['guess_value']) + " left")
-    guessLeft = str(session['guess_value'])
-    return jsonify({'wrong' : Message, 'guesses' : guessLeft})
+		elif session['random_value'] > guess:
+			session['guess_value'] -= 1
+			if session['guess_value'] == 0:
+				session['victory'] -= 1
+				score = session['victory']
+				Message = ("Sadly you ran out of guesses, the number was " + str(session['random_value']) + ". please try again ")
+				return jsonify({'error' : Message, 'guesses' : '0', 'score': score})
+			else:
+				Message = (str(guess) + " is to low! You have " + str(session['guess_value']) + " left")
+				guessLeft = str(session['guess_value'])
+				return jsonify({'wrong' : Message, 'guesses' : guessLeft})
 
-  elif session['random_value'] < guess:
-   session['guess_value'] -= 1
-   if session['guess_value'] == 0:
-    session['victory'] -= 1
-    score = session['victory']
-    Message = ("Sadly you ran out of guesses, the number was " + str(session['random_value']) + ". please try again ")
-    return jsonify({'error' : Message, 'guesses' : '0', 'score': score})
-   else:
-    Message = (str(guess) + " is to high! You have " + str(session['guess_value'])  + " left")
-    guessLeft = str(session['guess_value'])
-    return jsonify({'wrong' : Message, 'guesses' : guessLeft})
+		elif session['random_value'] < guess:
+			session['guess_value'] -= 1
+			if session['guess_value'] == 0:
+				session['victory'] -= 1
+				score = session['victory']
+				Message = ("Sadly you ran out of guesses, the number was " + str(session['random_value']) + ". please try again ")
+				return jsonify({'error' : Message, 'guesses' : '0', 'score': score})
+			else:
+				Message = (str(guess) + " is to high! You have " + str(session['guess_value'])  + " left")
+				guessLeft = str(session['guess_value'])
+				return jsonify({'wrong' : Message, 'guesses' : guessLeft})
 
- return jsonify({'error' : 'Some data is missing, please try again!'})
+	return jsonify({'error' : 'Some data is missing, please try again!'})
 
 if __name__ == "__main__":
     app.debug = True
