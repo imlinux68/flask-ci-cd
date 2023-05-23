6b442049ab8261c2ae802625035d03e7e65ac5d9
imlinux68 <104739714+imlinux68@users.noreply.github.com>
2023-05-23 09:53:25 +0300

    Create creator.sh

---
+++ creator.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+mkdir -p CI_CD_Project/app/{static/{css,js},templates}
+touch CI_CD_Project/app/instruction.md CI_CD_Project/app/requirements.txt CI_CD_Project/app/run.py CI_CD_Project/app/static/css/design.css CI_CD_Project/app/static/js/form.js CI_CD_Project/app/templates/guess.html
